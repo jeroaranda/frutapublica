ec4030ac04d616e768dd11a2fdc0f2737273d9a2
Jero <jeroaranda@gmail.com>
2024-04-24 23:33:27 +0000

    fix path

--- pages/comparte_fruta.py
+++ pages/comparte_fruta.py
@@ -34,7 +34,7 @@ img_file_buffer = st.camera_input("Toma una foto")
 
 if img_file_buffer is not None:
     # To read image file buffer as bytes:
-    with open (f'/data/imagenes/{str(id)}.jpg','wb') as file:
+    with open (f'{str(id)}.jpg','wb') as file:
           file.write(img_file_buffer.getbuffer())
     bytes_data = img_file_buffer.getvalue()
     
