a5cc5dde7caab64e18e36f27ed9d86945f822365
Jero <jeroaranda@gmail.com>
2024-04-24 22:43:21 +0000

    fix latitude key

--- pages/comparte_fruta.py
+++ pages/comparte_fruta.py
@@ -13,7 +13,7 @@ with st.form("myform"):
     placeholder_for_optional_text = st.empty()
 
     location = streamlit_geolocation()
-    if latitude in location:
+    if 'latitude' in location:
         st.write(f'Your location is (lat:{location.latitude},lon:{location.longitude})')
     img_file_buffer = st.camera_input("Toma una foto")
 
