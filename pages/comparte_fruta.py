c1473f936160c1a63834e7da735570c08b6f0590
Jero <jeroaranda@gmail.com>
2024-04-24 23:05:25 +0000

    remove form

--- pages/comparte_fruta.py
+++ pages/comparte_fruta.py
@@ -3,41 +3,43 @@ from streamlit_geolocation import streamlit_geolocation
 
 st.title('Comparte fruta')
 
-# Layout for the form 
-with st.form("myform"):
+# Layout for the form
 
-    "### A form"
+"### Formulario para capturar flora aprovechable"
 
-    # These exist within the form but won't wait for the submit button
-    placeholder_for_selectbox = st.empty()
-    placeholder_for_optional_text = st.empty()
+options = ['limón','mango','mandarina','gasparito'] + ["Otra opción..."]
+producto = st.selectbox("Selecciona una opción", options=options)
 
-    location = streamlit_geolocation()
-    if 'latitude' in location:
-        st.write(f'Your location is (lat:{location["latitude"]},lon:{location["longitude"]})')
-    img_file_buffer = st.camera_input("Toma una foto")
+if producto == "Otra opción...":
+    otroproducto = st.text_input("Añade tu opción...")
 
-    if img_file_buffer is not None:
-        # To read image file buffer as bytes:
-        bytes_data = img_file_buffer.getvalue()
-        # Check the type of bytes_data:
-        # Should output: <class 'bytes'>
-        st.write(type(bytes_data))
+options = ['jero','nacho'] + ["Otra opción..."]
+usuario = st.selectbox("Selecciona una opción", options=options)
+
+if usuario == "Otra opción...":
+    otrousuario = st.text_input("Añade tu opción...")
+
+location = streamlit_geolocation()
+if location['latitude'] != None:
+    st.write(f'Your location is (lat:{location["latitude"]},lon:{location["longitude"]})')
+img_file_buffer = st.camera_input("Toma una foto")
+
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
+#fecha
+observaciones = st.text_input('Observaciones')
+
+if st.button("Capturar flora", type="primary"):
+    st.write('Capturando')
+
 
     
-    submit_button = st.form_submit_button("Submit!")
-
     
 
-    # Create selectbox
-    with placeholder_for_selectbox:
-        options = ['limón','mango','mandarina','gasparito'] + ["Another option..."]
-        selection = st.selectbox("Selecciona una opción", options=options)
-
-    # Create text input for user entry
-    with placeholder_for_optional_text:
-        if selection == "Otra opción...":
-            otherOption = st.text_input("Añade tu opción...")
 
     
 
