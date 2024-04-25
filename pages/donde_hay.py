55d0888122800e82418343326e1b89e7500e270e
Jero <jeroaranda@gmail.com>
2024-04-25 22:17:32 +0000

    fix hover display

--- pages/donde_hay.py
+++ pages/donde_hay.py
@@ -14,11 +14,11 @@ st.dataframe(df[['lat','lon','flora inferida']])
 #px.set_mapbox_access_token(open(".mapbox_token").read())
 df['size'] = .7
 fig = px.scatter_mapbox(df, lat="lat", lon="lon",  size='size',   color="flora inferida",mapbox_style="carto-positron",zoom=.7,
-        hover_data={
-        "Flora": "flora inferida",  # Display "Flora" instead of the full column name
-        "Usuario": "usuario",
-        "Observations": "observaciones"
-    })
+        hover_data=[
+        "flora inferida",  # Display "Flora" instead of the full column name
+        "usuario",
+        "observaciones"]
+    )
 fig.update_traces(cluster=dict(enabled=True))
 fig.show()
 st.plotly_chart(fig, use_container_width=True) 
