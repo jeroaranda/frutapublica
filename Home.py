ee12edb2d46190deb09371c09ef77dcce1e5dc20
jero <jeroaranda@gmail.com>
2025-02-20 16:38:21 -0600

    modify size

--- Home.py
+++ Home.py
@@ -103,7 +103,7 @@ def show_map_view():
     
     df = get_observations_df()
     df['shortdescription'] = df['description'].apply(lambda x: x[:40] + '...' if len(x) > 40 else x)
-    df['size'] = 10
+    df['size'] = 100
     st.warning(df.shape)
     st.warning(df[['lat','lon']].head())
 
