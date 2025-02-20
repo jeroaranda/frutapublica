473fd0412ab27f388a6ca7ffe414e68ea2ac7c81
jero <jeroaranda@gmail.com>
2025-02-20 16:29:20 -0600

    provide warnings

--- Home.py
+++ Home.py
@@ -104,7 +104,8 @@ def show_map_view():
     df = get_observations_df()
     df['shortdescription'] = df['description'].apply(lambda x: x[:40] + '...' if len(x) > 40 else x)
     df['size'] = 10
-    
+    st.warning(df.shape)
+    st.warning(df.describe())
     # Your original map code
     fig = px.scatter_mapbox(
         df,
